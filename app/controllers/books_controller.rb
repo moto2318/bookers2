460ad7ed70215d933f2b3dc25e7b6c6bdf773f4c
EC2 Default User <suigekka0101@gmail.com>
2023-08-11 22:00:00 +0900

    bookers2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,14 +13,15 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    if @book.save
-      flash[:notice] = "Signed in successfully."
-      redirect_to book_path(@book.id)
-    else
-      @books = Book.all
-      render :index
-    end
+  if @book.save
+    flash[:notice] = "Book created successfully."
+    redirect_to book_path(@book.id)
+  else
+    @books = Book.all
+    render :index
   end
+end
+
 
   def edit
     @book = Book.find(params[:id])
