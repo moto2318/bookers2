6471ac28b88e0a15667ddced46e832623baa5693
EC2 Default User <ec2-user@ip-172-31-31-237.ap-northeast-1.compute.internal>
2023-08-22 16:43:53 +0000

    Initial Commit

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,13 @@
+class BooksController < ApplicationController
+  def new
+  end
+
+  def crate
+  end
+
+  def index
+  end
+
+  def show
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  get 'users/show'
+  get 'users/edit'
+  get 'books/new'
+  get 'books/crate'
+  get 'books/index'
+  get 'books/show'
+  devise_for :users
+  root to: "homes#top"
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
