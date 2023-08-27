4ad15cee3e599e139a0931ac75e04cf76280b08a
EC2 Default User <ec2-user@ip-172-31-31-237.ap-northeast-1.compute.internal>
2023-08-27 22:22:11 +0000

    30-41

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,11 +3,32 @@ class BooksController < ApplicationController
   end
 
   def crate
+    @book = Book.new(book_params)
+    @book.user.id = current_user.id
+    @book.save
+    redirect_to '/book'
   end
 
   def index
+    @books = Book.all
   end
 
   def show
   end
+  
+  def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(list_params)
+    redirect_to book_path(book.id)
+  end
+  
+  private
+  def book_params
+    params.require(:list).permit(:title, :body, :image)
+  end
+  
 end
