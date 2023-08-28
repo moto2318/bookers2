b23a81194482322c5acf7e441bfc58c7dac59efb
EC2 Default User <ec2-user@ip-172-31-31-237.ap-northeast-1.compute.internal>
2023-08-28 09:21:33 +0000

    Errors eliminated by more than half

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,50 +1,48 @@
 class BooksController < ApplicationController
-  def new
-  end
-
-  def create
-    @book = Book.new(book_params)
-    @book.user.id = current_user.id
-    if @book.save
-      redirect_to '/book'
-    else
-      render :index
-    end
-  end
 
   def index
     @books = Book.all
   end
 
   def show
-  end
-  
-  def edit
-    is_matching_login_user
     @book = Book.find(params[:id])
-    
   end
-  
-  def update
-    is_matching_login_user
-    book = Book.find(params[:id])
-    if book.update(list_params)
-    redirect_to book_path(book.id)
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      redirect_to book_path(@book.id)
     else
-    render :edit_book
+      @books = Book.all
+      render :index
     end
   end
-  
+
+  def edit
+    @book = Book.find(params[:id])
+    is_matching_login_user(@book)
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    is_matching_login_user(@book)
+    if @book.update(book_params)
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
+  end
+
   private
+
   def book_params
-    params.require(:book).permit(:title, :body, :image)
+    params.require(:book).permit(:title, :body)
   end
-  
-  def is_matching_login_user
-    book = Book.find(params[:id])
-    unless book.id == current_user.id
-      redirect_to post_images_path
+
+  def is_matching_login_user(book)
+    unless book.user == current_user
+      redirect_to books_path
     end
   end
-  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_08_27_103016) do
+ActiveRecord::Schema.define(version: 2023_08_28_070210) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -41,8 +41,9 @@ ActiveRecord::Schema.define(version: 2023_08_27_103016) do
   end
 
   create_table "books", force: :cascade do |t|
-    t.text "title"
+    t.string "title"
     t.text "body"
+    t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
